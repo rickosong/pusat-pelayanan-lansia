23c88841bec87dab47743619a63b0c859a09a4ab
rickosong <ricko.aprilianto00@gmail.com>
2021-12-08 19:53:24 +0800

    the last commit (i think)

---
+++ wp-content/themes/tes tema/front-page.php
@@ -0,0 +1,12 @@
+<?= get_header(); ?>
+
+<div class="jumbotron">
+    <h1>Selamat datang</h1>
+    <p>Website ini menyediakan semua berita tentang Pandemi Covid 19</p>
+    <p>Untuk melihat beritanya silahkan</a>
+    <br><br><br>
+    <a href="http://localhost/pusat-pelayanan-lansia/index.php/laman-contoh/"><button>Masuk Ke Sini</button></a>
+</div>
+
+
+<?= get_footer(); ?>2
